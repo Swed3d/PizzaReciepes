7aaa30efd82075a3a314801ad7b52bf2c78c6184
Julia Khodzko <j.khodzko@gmail.com>
2021-04-26 14:32:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/pizzareciepes/Utils.java
@@ -0,0 +1,129 @@
+package com.example.pizzareciepes;
+
+public class Utils {
+    public static final String PIZZA_1_TITLE = "Next level Margherita pizza";
+    public static final String PIZZA_2_TITLE = "Pizza Margherita in 4 easy steps";
+    public static final String PIZZA_3_TITLE = "Caramelised onion & goat’s cheese pizza";
+    public static final String PIZZA_4_TITLE = "Superhealthy pizza";
+    public static final String PIZZA_5_TITLE = "Frying pan pizza";
+    public static final String PIZZA_6_TITLE = "Egg & rocket pizzas";
+    public static final String PIZZA_7_TITLE = "Next level Margherita pizza";
+    public static final String PIZZA_8_TITLE = "Sourdough pizza";
+    public static final String PIZZA_9_TITLE = "Chicken tikka masala pizzas";
+    public static final String PIZZA_1_DESCRIPTION = "Forget takeaways – you can’t beat a homemade Margherita pizza topped with fresh tomato sauce and melted cheese. Here's how to master this everyday classic";
+    public static final String PIZZA_2_DESCRIPTION = "Even a novice cook can master the art of pizza with our simple step-by-step guide. Bellissimo!";
+    public static final String PIZZA_3_DESCRIPTION = "Make a veggie pizza that's healthy and low-fat instead of ordering a takeaway. This easy recipe uses goat's cheese for tang while the onions give it sweetness";
+    public static final String PIZZA_4_DESCRIPTION = "The quantities for this are generous, so if you have any leftovers, pop a wedge of cold pizza into your lunchbox the next day";
+    public static final String PIZZA_5_DESCRIPTION = "Use seeded tortillas as pizza bases for a quick and healthy lunch - crack an egg in the centre and bake to boost protein intake";
+    public static final String PIZZA_6_DESCRIPTION = "Make homemade sourdough pizza with a wonderfully chewy crust. Try our margherita recipe, then customise with your favourite toppings";
+    public static final String PIZZA_7_DESCRIPTION = "Rustle up our spiced chicken pizzas using naan bread in just 20 minutes. To balance the heat, serve them with dollops of mango chutney and natural yogurt";
+    public static final String PIZZA_8_DESCRIPTION = "Take your senses on a trip to Italy with this authentic pizza Margherita";
+    public static final String PIZZA_9_DESCRIPTION = "Make pizza for the family with a homemade base and tomato sauce. The recipe is perfect to get kids involved in cooking. Top with mozzarella and fresh basil";
+    public static final String PIZZA_1_RECIPE = "STEP 1\n" +
+            "Make the base: Put the flour into a large bowl, then stir in the yeast and salt. Make a well, pour in 200ml warm water and the olive oil and bring together with a wooden spoon until you have a soft, fairly wet dough. Turn onto a lightly floured surface and knead for 5 mins until smooth. Cover with a tea towel and set aside. You can leave the dough to rise if you like, but it’s not essential for a thin crust.\n" +
+            "\n" +
+            "STEP 2\n" +
+            "Make the sauce: Mix the passata, basil and crushed garlic together, then season to taste. Leave to stand at room temperature while you get on with shaping the base.\n" +
+            "\n" +
+            "STEP 3\n" +
+            "Roll out the dough: if you’ve let the dough rise, give it a quick knead, then split into two balls. On a floured surface, roll out the dough into large rounds, about 25cm across, using a rolling pin. The dough needs to be very thin as it will rise in the oven. Lift the rounds onto two floured baking sheets.\n" +
+            "\n" +
+            "STEP 4\n" +
+            "Top and bake: heat the oven to 240C/220C fan/gas 8. Put another baking sheet or an upturned baking tray in the oven on the top shelf. Smooth sauce over bases with the back of a spoon. Scatter with cheese and tomatoes, drizzle with olive oil and season. Put one pizza, still on its baking sheet, on top of the preheated sheet or tray. Bake for 8-10 mins until crisp. Serve with a little more olive oil, and basil leaves if using. Repeat step for remaining pizza.";
+    public static final String PIZZA_2_RECIPE = "STEP 1\n" +
+            "Heat oven to 220C/200C fan/gas 7. Tip the flour into a mixer with a dough hook, or a bowl. Add the yeast, salt, oil and just under 100ml warm water then mix to a soft dough. Knead in the food mixer for about 5 mins, but if making this by hand, tip onto a work surface and knead for about 10 mins. The dough is sticky, but try not to add too much extra flour. Leave in the bowl and cover with a tea towel while you make the topping. There is no need to let the dough prove for a specific time – just let it sit while you get on with the next step.\n" +
+            "\n" +
+            "STEP 2\n" +
+            "Tip the onions into a non-stick wok and add the oil, 4 tbsp water and balsamic vinegar. Cover with a saucepan lid that sits inside the pan to help the onions soften, then cook for 15 mins, stirring about 3 times and replacing the lid quickly so as not to lose too much moisture. After the time is up, the onions should be golden and all the liquid gone. Tip onto a plate. Add the spinach and garlic to the pan and stir-fry until the spinach has wilted.\n" +
+            "\n" +
+            "STEP 3\n" +
+            "Take the dough from the bowl and cut in half with an oiled knife, then press each piece into a 25-15 cm oval on a large greased baking sheet with oiled hands. Don’t knead the dough first otherwise it will be too elastic and it will keep shrinking back.\n" +
+            "\n" +
+            "STEP 4\n" +
+            "Spread with the spinach followed by the onions, then dot with the cheese and scatter with the olives, thyme and sunflower seeds. Bake for 15 mins until golden and the base is cooked through.";
+    public static final String PIZZA_3_RECIPE = "STEP 1\n" +
+            "Mix the flours and yeast with a pinch of salt in a food processor fitted with a dough blade. Pour in the water and mix to a soft dough, then work for 1 min. Remove the dough and roll out on a lightly floured surface to a round about 30cm across. Lift onto an oiled baking sheet.\n" +
+            "\n" +
+            "STEP 2\n" +
+            "Spread the canned tomatoes over the dough to within 2cm of the edges. Arrange the cherry tomatoes and courgettes over the top, then scatter with the mozzarella. Mix the capers, olives and garlic, then scatter over the top. Drizzle evenly with the oil. Leave to rise for 20 mins. Heat oven to 240C/ fan 220C/gas 9 or the highest setting.\n" +
+            "\n" +
+            "STEP 3\n" +
+            "Bake the pizza for 10-12 mins until crisp and golden around the edges. Scatter with the parsley to serve.";
+    public static final String PIZZA_4_RECIPE = "STEP 1\n" +
+            "Heat oven to 220C/fan 200C/gas 7. Place the pepper, courgette and red onion on a large non-stick baking tray and drizzle with 1 tsp olive oil. Season well and toss to combine. Roast in the oven for 20 mins until soft and beginning to brown. Set aside.\n" +
+            "\n" +
+            "STEP 2\n" +
+            "Heat the grill to medium. Season the flour well and, in a large bowl, mix with the remaining oil and 4-5 tbsp water to form a soft dough. Knead briefly, then roll out on a floured surface to a rough 20cm circle. Transfer the dough to a large ovenproof, non-stick frying pan and fry over a medium heat for 5 mins, until the underside begins to brown. Turn over and cook for a further 5 mins. Spread the tomato sauce over the base, scatter with the roasted veg, then sprinkle with cheddar. Grill the pizza for 3-4 mins until cheese has melted. Serve immediately, sliced into wedges, with a large green salad.";
+    public static final String PIZZA_5_RECIPE = "STEP 1\n" +
+            "Tip the flour into a bowl, then stir in the yeast and 1 tsp salt. Make a well in the centre and pour in 200ml warm water (make sure it’s not too hot) along with the oil. Stir together with a wooden spoon until you have a soft, fairly wet dough.\n" +
+            "\n" +
+            "STEP 2\n" +
+            "Tip the dough out onto a lightly floured surface and knead for 5 mins until smooth. Cover with a tea towel and set aside for an hour or so or until the dough has puffed up and doubled in size. You can also leave the rough, unkneaded dough in the bowl, cover with a tea towel and leave in the fridge overnight and the dough will continue to prove on its own.\n" +
+            "\n" +
+            "STEP 3\n" +
+            "Meanwhile, make the tomato sauce. Put the oil in a small pan and fry the garlic briefly (don’t let it brown), then add the passata and simmer everything until the sauce thickens a little. Leave to cool.\n" +
+            "\n" +
+            "STEP 4\n" +
+            "Once the dough has risen, knead it quickly in the bowl to knock it back, then tip out onto a lightly floured surface and cut into two balls. Roll out each ball into a large teardrop that is very thin and about 25cm across (teardrop shapes fit baking sheets more easily than rounds). \n" +
+            "\n" +
+            "STEP 5\n" +
+            "Heat oven to 240C/220C fan/ gas 9 with a large baking sheet inside. Lift one of the bases onto another floured baking sheet. Smooth the sauce over the base with the back of a spoon, scatter over half the mozzarella, drizzle with olive oil and season. Put the pizza, still on its baking sheet, on top of the hot sheet in the oven and bake for 8-10 mins until crisp. ";
+    public static final String PIZZA_6_RECIPE = "STEP 1\n" +
+            "Tip the flour into a bowl, then stir in the yeast and 1 tsp salt. Make a well in the centre and pour in 200ml warm water (make sure it’s not too hot) along with the oil. Stir together with a wooden spoon until you have a soft, fairly wet dough.\n" +
+            "\n" +
+            "STEP 2\n" +
+            "Tip the dough out onto a lightly floured surface and knead for 5 mins until smooth. Cover with a tea towel and set aside for an hour or so or until the dough has puffed up and doubled in size. You can also leave the rough, unkneaded dough in the bowl, cover with a tea towel and leave in the fridge overnight and the dough will continue to prove on its own.\n" +
+            "\n" +
+            "STEP 3\n" +
+            "Meanwhile, make the tomato sauce. Put the oil in a small pan and fry the garlic briefly (don’t let it brown), then add the passata and simmer everything until the sauce thickens a little. Leave to cool.\n" +
+            "\n" +
+            "STEP 4\n" +
+            "Once the dough has risen, knead it quickly in the bowl to knock it back, then tip out onto a lightly floured surface and cut into two balls. Roll out each ball into a large teardrop that is very thin and about 25cm across (teardrop shapes fit baking sheets more easily than rounds). \n" +
+            "\n" +
+            "STEP 5\n" +
+            "Heat oven to 240C/220C fan/ gas 9 with a large baking sheet inside. Lift one of the bases onto another floured baking sheet. Smooth the sauce over the base with the back of a spoon, scatter over half the mozzarella, drizzle with olive oil and season. Put the pizza, still on its baking sheet, on top of the hot sheet in the oven and bake for 8-10 mins until crisp. ";
+    public static final String PIZZA_7_RECIPE = "STEP 1\n" +
+            "Tip the flour into a bowl, then stir in the yeast and 1 tsp salt. Make a well in the centre and pour in 200ml warm water (make sure it’s not too hot) along with the oil. Stir together with a wooden spoon until you have a soft, fairly wet dough.\n" +
+            "\n" +
+            "STEP 2\n" +
+            "Tip the dough out onto a lightly floured surface and knead for 5 mins until smooth. Cover with a tea towel and set aside for an hour or so or until the dough has puffed up and doubled in size. You can also leave the rough, unkneaded dough in the bowl, cover with a tea towel and leave in the fridge overnight and the dough will continue to prove on its own.\n" +
+            "\n" +
+            "STEP 3\n" +
+            "Meanwhile, make the tomato sauce. Put the oil in a small pan and fry the garlic briefly (don’t let it brown), then add the passata and simmer everything until the sauce thickens a little. Leave to cool.\n" +
+            "\n" +
+            "STEP 4\n" +
+            "Once the dough has risen, knead it quickly in the bowl to knock it back, then tip out onto a lightly floured surface and cut into two balls. Roll out each ball into a large teardrop that is very thin and about 25cm across (teardrop shapes fit baking sheets more easily than rounds). \n" +
+            "\n" +
+            "STEP 5\n" +
+            "Heat oven to 240C/220C fan/ gas 9 with a large baking sheet inside. Lift one of the bases onto another floured baking sheet. Smooth the sauce over the base with the back of a spoon, scatter over half the mozzarella, drizzle with olive oil and season. Put the pizza, still on its baking sheet, on top of the hot sheet in the oven and bake for 8-10 mins until crisp. ";
+    public static final String PIZZA_8_RECIPE = "STEP 1\n" +
+            "Tip the flour into a bowl, then stir in the yeast and 1 tsp salt. Make a well in the centre and pour in 200ml warm water (make sure it’s not too hot) along with the oil. Stir together with a wooden spoon until you have a soft, fairly wet dough.\n" +
+            "\n" +
+            "STEP 2\n" +
+            "Tip the dough out onto a lightly floured surface and knead for 5 mins until smooth. Cover with a tea towel and set aside for an hour or so or until the dough has puffed up and doubled in size. You can also leave the rough, unkneaded dough in the bowl, cover with a tea towel and leave in the fridge overnight and the dough will continue to prove on its own.\n" +
+            "\n" +
+            "STEP 3\n" +
+            "Meanwhile, make the tomato sauce. Put the oil in a small pan and fry the garlic briefly (don’t let it brown), then add the passata and simmer everything until the sauce thickens a little. Leave to cool.\n" +
+            "\n" +
+            "STEP 4\n" +
+            "Once the dough has risen, knead it quickly in the bowl to knock it back, then tip out onto a lightly floured surface and cut into two balls. Roll out each ball into a large teardrop that is very thin and about 25cm across (teardrop shapes fit baking sheets more easily than rounds). \n" +
+            "\n" +
+            "STEP 5\n" +
+            "Heat oven to 240C/220C fan/ gas 9 with a large baking sheet inside. Lift one of the bases onto another floured baking sheet. Smooth the sauce over the base with the back of a spoon, scatter over half the mozzarella, drizzle with olive oil and season. Put the pizza, still on its baking sheet, on top of the hot sheet in the oven and bake for 8-10 mins until crisp. ";
+    public static final String PIZZA_9_RECIPE = "STEP 1\n" +
+            "Tip the flour into a bowl, then stir in the yeast and 1 tsp salt. Make a well in the centre and pour in 200ml warm water (make sure it’s not too hot) along with the oil. Stir together with a wooden spoon until you have a soft, fairly wet dough.\n" +
+            "\n" +
+            "STEP 2\n" +
+            "Tip the dough out onto a lightly floured surface and knead for 5 mins until smooth. Cover with a tea towel and set aside for an hour or so or until the dough has puffed up and doubled in size. You can also leave the rough, unkneaded dough in the bowl, cover with a tea towel and leave in the fridge overnight and the dough will continue to prove on its own.\n" +
+            "\n" +
+            "STEP 3\n" +
+            "Meanwhile, make the tomato sauce. Put the oil in a small pan and fry the garlic briefly (don’t let it brown), then add the passata and simmer everything until the sauce thickens a little. Leave to cool.\n" +
+            "\n" +
+            "STEP 4\n" +
+            "Once the dough has risen, knead it quickly in the bowl to knock it back, then tip out onto a lightly floured surface and cut into two balls. Roll out each ball into a large teardrop that is very thin and about 25cm across (teardrop shapes fit baking sheets more easily than rounds). \n" +
+            "\n" +
+            "STEP 5\n" +
+            "Heat oven to 240C/220C fan/ gas 9 with a large baking sheet inside. Lift one of the bases onto another floured baking sheet. Smooth the sauce over the base with the back of a spoon, scatter over half the mozzarella, drizzle with olive oil and season. Put the pizza, still on its baking sheet, on top of the hot sheet in the oven and bake for 8-10 mins until crisp. ";
+
+
+}
